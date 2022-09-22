3a71075848851330603c0d8df2d6ef76c652ec57
alexander <axldeth@gmail.com>
2022-09-22 14:06:14 -0400

    Se agrego otro componente y se solucionaron problemas con el media query

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
@@ -13,4 +16,10 @@ function toggleDesktopMenu() {
         desktopMenu.classList.toggle('inactive');
     } 
 */
+}
+
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+
 }
