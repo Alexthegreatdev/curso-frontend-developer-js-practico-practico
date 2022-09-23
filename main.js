a7b89ca29cec93cf99e5dc7bd26ee45c95d36a0e
alexander <axldeth@gmail.com>
2022-09-23 15:06:24 -0400

    Se agrego otro componente y se soluciono un problema de logica sobre las listas que se desplegan

--- main.js
+++ main.js
@@ -2,24 +2,42 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+menuEmail.addEventListener('click', () => {
+    toggleClass(desktopMenu);
+});
+burgerMenu.addEventListener('click', () => {
+    toggleClass(mobileMenu);
+});
+menuCarritoIcon.addEventListener('click', () => {
+    toggleClass(aside);
+});
 
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-burgerMenu.addEventListener('click', toggleMobileMenu);
-
-function toggleDesktopMenu() {
+/* function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
- /*   
+   
     if (desktopMenu.className.includes('inactive')) {
         desktopMenu.classList.toggle('inactive');
     } else {
         desktopMenu.classList.toggle('inactive');
     } 
-*/
-}
 
+}
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+} */
 
+/* Mi solucion para que solo se pueda abrir uno */
+function toggleClass(openElement) {
+    let inactiveItems = [desktopMenu, mobileMenu, aside];
+        for (const item of inactiveItems) {
+            if (item != openElement  && !item.classList.contains('inactive')) {
+                item.classList.toggle('inactive');
+            }
+        }
+    openElement.classList.toggle('inactive');
 }
